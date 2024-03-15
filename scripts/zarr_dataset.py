c4dc08646242b513787271dfc0327d7a03b6cf13
david <david.moser@gmx.net>
2024-03-15 13:38:10 +0100

    zarr dataset: more work on csv reading

--- scripts/zarr_dataset.py
+++ scripts/zarr_dataset.py
@@ -1,15 +1,22 @@
-import pandas
+import numpy as np
+import pandas as pd
 import zarr
-from torch.utils.data import Dataset
+from torch.utils.data import Dataset, Subset
 
 
 class ZarrDataset(Dataset):
-    def __init__(self, anno_file, zarr_file, zarr_path):
-        self.annotations = pandas.read_csv(anno_file)
+    def __init__(self, anno_file, anno_cols, zarr_file, zarr_path):
+        df = pd.read_csv(anno_file, sep='\t', quotechar='$', low_memory=False, on_bad_lines='warn', na_values='..')
+        self.labels = df[anno_cols].values
         self.zarr = zarr.open(zarr_file, mode='r')[zarr_path]
 
     def __len__(self):
-        return len(self.annotations)
+        return len(self.labels)
 
     def __getitem__(self, index):
-        return self.zarr[index], self.annotations.iloc[index]
+        return self.zarr[index], self.labels[index]
+
+    def filter_nan(self):
+        mask = np.all(~np.isnan(self.labels), axis=1)
+        indices = mask.nonzero()[0]
+        return Subset(self, indices)
