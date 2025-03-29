4c2c119168b43a7632861098d9ca5b931daf132c
David Moser <david.moser@gmx.net>
2025-03-29 16:46:17 +0100

    add dropout

--- scripts/genonet.py
+++ scripts/genonet.py
@@ -1,32 +1,35 @@
 import numpy as np
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 import torch.nn.init as init
 
 
-def create_layer(in_dim, out_dim, batch_norm=False):
+def create_layer(in_dim, out_dim, batch_norm=False, dropout=0.0):
     layers = [nn.Linear(in_dim, out_dim)]
     init.kaiming_uniform_(layers[0].weight, nonlinearity='relu')
     init.zeros_(layers[0].bias)
     if batch_norm:
         layers.append(nn.BatchNorm1d(out_dim))
+    layers.append(nn.ReLU())
+    if dropout > 0.0:
+        layers.append(nn.Dropout(dropout))
     return nn.Sequential(*layers)
 
 
 class Genonet(nn.Module):
     def __init__(self, input_dim, output_dim, hidden_dim, hidden_layers, first_fun=lambda x: x, final_fun=torch.tanh,
-                 batch_norm=False):
+                 batch_norm=False, dropout=0.0):
         super(Genonet, self).__init__()
         # Initial conversion
         self.first_fun = first_fun
 
         # Initial layer from n to m dimensions
-        self.initial_layer = create_layer(input_dim, hidden_dim, batch_norm)
+        self.initial_layer = create_layer(input_dim, hidden_dim, batch_norm, dropout)
 
         # Intermediate layers (k of them, each from m to m dimensions)
         self.intermediate_layers = nn.ModuleList(
-            [create_layer(hidden_dim, hidden_dim, batch_norm) for _ in range(hidden_layers)])
+            [create_layer(hidden_dim, hidden_dim, batch_norm, dropout) for _ in range(hidden_layers)]
+        )
 
         # Final layer to 3 dimensions, no batch norm here
         self.final_layer = nn.Linear(hidden_dim, output_dim)
@@ -39,11 +42,11 @@ class Genonet(nn.Module):
         x = self.first_fun(x)
 
         # Pass through the initial layer
-        x = F.relu(self.initial_layer(x))
+        x = self.initial_layer(x)
 
         # Pass through each intermediate layer with ReLU activation
         for layer in self.intermediate_layers:
-            x = F.relu(layer(x))
+            x = layer(x)
 
         # Pass through the final layer
         x = self.final_layer(x)
